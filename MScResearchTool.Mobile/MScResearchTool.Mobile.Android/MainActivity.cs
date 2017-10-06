b612e2bc3d676c635135e03d8ac6f8588638e922
Zbigniew Marszolik <z.marszolik@gmail.com>
2017-10-06 08:13:40 +0200

    Android solution created - empty projects created.

---
+++ MScResearchTool.Mobile/MScResearchTool.Mobile.Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace MScResearchTool.Mobile.Android
+{
+    [Activity(Label = "MScResearchTool.Mobile.Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
