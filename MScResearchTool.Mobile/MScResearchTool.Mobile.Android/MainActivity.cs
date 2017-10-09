0bd21b1b45c26108327fc75e046f4c9b2c14d2d4
Zbigniew Marszolik <z.marszolik@gmail.com>
2017-10-09 11:03:44 +0200

    [ANDROID] Autofac installed and configured. XUnit installed to Test project.

--- MScResearchTool.Mobile/MScResearchTool.Mobile.Android/MainActivity.cs
+++ MScResearchTool.Mobile/MScResearchTool.Mobile.Android/MainActivity.cs
@@ -1,17 +1,31 @@
 ﻿using Android.App;
 using Android.OS;
+using Autofac;
+using MScResearchTool.Mobile.Binder.AutofacModules;
 
 namespace MScResearchTool.Mobile.Android
 {
     [Activity(Label = "MScResearchTool.Mobile.Android", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        private IContainer _container { get; set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            ContainerSetup();
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+        }
+
+        private void ContainerSetup()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<ServicesModule>();
+            _container = builder.Build();
         }
     }
 }
---
+++ MScResearchTool.Mobile/MScResearchTool.Mobile.Binder/AutofacModules/ServicesModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace MScResearchTool.Mobile.Binder.AutofacModules
+{
+    public class ServicesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+        }
+    }
+}
