0c493e066dd722f046a04cd359d0931a0b418084
Zbigniew Marszolik <z.marszolik@gmail.com>
2017-10-12 13:24:29 +0200

    Removed initially auto-generated views that will not be used, changed other views' encoding to UTF-8. Also redesigned error page - that error page is ready now together with its backend logic.

---
+++ MScResearchTool.Server/MScResearchTool.Server.Web/AutofacModules/WebModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace MScResearchTool.Server.Web.AutofacModules
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+        }
+    }
+}
