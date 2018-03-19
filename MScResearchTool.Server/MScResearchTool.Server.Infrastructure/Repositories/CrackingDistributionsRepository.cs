7b079bd83467ba7659753ca8a23103a4b11d31f1
Zbigniew Marszolik <z.marszolik@gmail.com>
2018-03-19 20:12:53 +0100

    FIxed packages issue with Tests.Web project. Implemented infrastructure layer for Cracking model.

--- MScResearchTool.Server/MScResearchTool.Server.Infrastructure/Repositories/CrackingDistributionsRepository.cs
+++ MScResearchTool.Server/MScResearchTool.Server.Infrastructure/Repositories/CrackingDistributionsRepository.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MScResearchTool.Server.Core.Models;
 using MScResearchTool.Server.Core.Repositories;
+using MScResearchTool.Server.Infrastructure.Connectors;
+using NHibernate.Linq;
 
 namespace MScResearchTool.Server.Infrastructure.Repositories
 {
@@ -8,27 +12,94 @@ namespace MScResearchTool.Server.Infrastructure.Repositories
     {
         public void Create(CrackingDistribution crackingDistribution)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void Delete(int crackingDistributionId)
-        {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    session.Save(crackingDistribution);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IList<CrackingDistribution> Read()
         {
-            throw new System.NotImplementedException();
+            IList<CrackingDistribution> crackingDistributions;
+
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    crackingDistributions = session.Query<CrackingDistribution>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                crackingDistributions = null;
+                throw ex;
+            }
+
+            return crackingDistributions;
         }
 
         public IList<CrackingDistribution> ReadEager()
         {
-            throw new System.NotImplementedException();
+            IList<CrackingDistribution> crackingDistributions;
+
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    crackingDistributions = session.Query<CrackingDistribution>().Fetch(x => x.Task).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                crackingDistributions = null;
+                throw ex;
+            }
+
+            return crackingDistributions;
         }
 
         public void Update(CrackingDistribution crackingDistribution)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    var tx = session.BeginTransaction();
+
+                    session.Update(crackingDistribution);
+
+                    tx.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int crackingDistributionId)
+        {
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    var distributionToDelete = session.Get("CrackingDistribution", crackingDistributionId);
+
+                    session.Delete(distributionToDelete);
+                    session.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
--- MScResearchTool.Server/MScResearchTool.Server.Infrastructure/Repositories/CrackingsRepository.cs
+++ MScResearchTool.Server/MScResearchTool.Server.Infrastructure/Repositories/CrackingsRepository.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MScResearchTool.Server.Core.Models;
 using MScResearchTool.Server.Core.Repositories;
+using MScResearchTool.Server.Infrastructure.Connectors;
+using NHibernate.Linq;
 
 namespace MScResearchTool.Server.Infrastructure.Repositories
 {
@@ -8,27 +12,94 @@ namespace MScResearchTool.Server.Infrastructure.Repositories
     {
         public void Create(Cracking crackingTask)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void Delete(int crackingTaskId)
-        {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    session.Save(crackingTask);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IList<Cracking> Read()
         {
-            throw new System.NotImplementedException();
+            IList<Cracking> crackingTasks;
+
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    crackingTasks = session.Query<Cracking>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                crackingTasks = null;
+                throw ex;
+            }
+
+            return crackingTasks;
         }
 
         public IList<Cracking> ReadEager()
         {
-            throw new System.NotImplementedException();
+            IList<Cracking> crackingTasks;
+
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    crackingTasks = session.Query<Cracking>().Fetch(x => x.Distributions).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                crackingTasks = null;
+                throw ex;
+            }
+
+            return crackingTasks;
         }
 
         public void Update(Cracking crackingTask)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    var tx = session.BeginTransaction();
+
+                    session.Update(crackingTask);
+
+                    tx.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int crackingTaskId)
+        {
+            try
+            {
+                using (var session = FluentNHibernateConnector.OpenSession())
+                {
+                    var taskToDelete = session.Get("Cracking", crackingTaskId);
+
+                    session.Delete(taskToDelete);
+                    session.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
