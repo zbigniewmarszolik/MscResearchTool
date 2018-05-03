97e397876b4a40051afe69c31ec1ad3fc541cbdc
Zbigniew Marszolik <z.marszolik@gmail.com>
2018-05-03 16:12:41 +0200

    Account creator fix.

--- MScResearchTool.AccountCreator/MScResearchTool.AccountCreator.Services/Services/ServiceBase.cs
+++ MScResearchTool.AccountCreator/MScResearchTool.AccountCreator.Services/Services/ServiceBase.cs
@@ -5,6 +5,6 @@ namespace MScResearchTool.AccountCreator.Services.Services
     public abstract class ServiceBase
     {
         protected HttpClient Client { get; set; }
-        protected static string ServerUrl = "http://localhost:80//Api/";
+        protected static string ServerUrl = "http://192.168.0.102//Api/";
     }
 }
